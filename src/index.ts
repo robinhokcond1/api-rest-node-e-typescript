5b5b87ab17114c2a398c50538ebba434b20f5636
Robson Ramos <robinhokcond1@gotmail.com>
2023-04-13 23:54:57 -0300

    feat: add folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from "./server/Server";
 
 server.listen(3333, () => {
-  console.log("APP rodando!");
+    console.log(`APP rodando na porta ${process.env.PORT || 3333}...`);
 });
