ca99047736534ad2a266b4d21e0f5e8043b0156b
Robson Ramos <robinhokcond1@gotmail.com>
2023-04-17 23:38:37 -0300

    feat: finishing validate request

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
+import { CidadesController } from "./../controllers";
 
 const router = Router();
 
 router.get("/", (req, res) => {
     return res.send("Olá, DEV!");
 });
-router.post("/teste", (req, res) => {
-    console.log(req.body);
-
-    return res.status(StatusCodes.ACCEPTED).json(req.body);
-});
+router.post(
+    "/cidades",
+    CidadesController.createValidation,
+    CidadesController.create
+);
 
 export { router };
