deb0e3fdf65ed267a90e7c54af8683c6581c6e8b
Robson Ramos <robinhokcond1@gotmail.com>
2023-04-18 22:09:30 -0300

    feat: finished validation request

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,29 +1,24 @@
-import { Request, RequestHandler, Response } from "express";
+import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 import { validation } from "../../shared/middleware";
 
 interface ICidade {
     nome: string;
-    estado: string;
-}
-
-interface IFilter {
-    filter?: string;
 }
 
 export const createValidation = validation((getSchema) => ({
-    body:getSchema<ICidade>(yup.object().shape({
-        nome: yup.string().required().min(3),
-        estado: yup.string().required().min(3),
-    })),
-    query: getSchema<IFilter>(yup.object().shape({
-        filter: yup.string().required().min(3),
-    })),
+    body: getSchema<ICidade>(
+        yup.object().shape({
+            nome: yup.string().required().min(3),
+        })
+    ),
 }));
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
     console.log(req.body);
 
-    return res.send("Create !");
+    return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send("Não implementado !");
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -7,10 +7,31 @@ const router = Router();
 router.get("/", (req, res) => {
     return res.send("Olá, DEV!");
 });
+
+router.get(
+    "/cidades",
+    CidadesController.getAllValidation,
+    CidadesController.getAll
+);
 router.post(
     "/cidades",
     CidadesController.createValidation,
     CidadesController.create
+);
+router.get(
+    "/cidades/:id",
+    CidadesController.getByIdValidation,
+    CidadesController.getById
+);
+router.put(
+    "/cidades/:id",
+    CidadesController.updateByIdValidation,
+    CidadesController.updateById
+);
+router.delete(
+    "/cidades/:id",
+    CidadesController.deleteByIdValidation,
+    CidadesController.deleteById
 );
 
 export { router };
